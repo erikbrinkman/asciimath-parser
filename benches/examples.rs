f9bf13356b75ea47584d3d6c6667b5b15423693b
Erik Brinkman <erik.brinkman@gmail.com>
2023-09-09 22:06:31 -0400

    cleanup

--- benches/examples.rs
+++ benches/examples.rs
@@ -1,14 +1,15 @@
 use asciimath_parser::ASCIIMATH_TOKENS;
-use lazy_static::lazy_static;
-use rand::distributions::{Alphanumeric, Slice};
+use rand::distr::Alphanumeric;
+use rand::distr::slice::Choose;
+use rand::distr::weighted::WeightedIndex;
 use rand::rngs::StdRng;
-use rand::{Rng, SeedableRng};
-use rand_distr::WeightedIndex;
+use rand::{Rng, RngExt, SeedableRng};
+use std::sync::LazyLock;
 
 pub const EXAMPLES: [&'static str; 1] = ["sum_(i=1)^n i^3=((n(n+1))/2)^2"];
 
 fn random_string<V>(rng: &mut impl Rng, tokens: &[(&str, V)]) -> String {
-    let token = Slice::new(tokens).unwrap();
+    let token = Choose::new(tokens).unwrap();
     let choice = WeightedIndex::new([1, 1, 3]).unwrap();
 
     let mut res = String::new();
@@ -23,9 +24,7 @@ fn random_string<V>(rng: &mut impl Rng, tokens: &[(&str, V)]) -> String {
     res
 }
 
-lazy_static! {
-    pub static ref RANDOM_EXAMPLES: [String; 30] = {
-        let mut rng = StdRng::from_seed([0; 32]);
-        [(); 30].map(|_| random_string(&mut rng, &ASCIIMATH_TOKENS))
-    };
-}
+pub static RANDOM_EXAMPLES: LazyLock<[String; 30]> = LazyLock::new(|| {
+    let mut rng = StdRng::from_seed([0; 32]);
+    [(); 30].map(|_| random_string(&mut rng, &ASCIIMATH_TOKENS))
+});
